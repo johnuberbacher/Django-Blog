2a225c930fad2e7724e71d9d90ade0528923bb84
John Uberbacher <johnuberbacher@gmail.com>
2020-08-08 16:13:48 -0600

    Blog Progress

---
+++ djangoBlog/djangoBlogApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomeView, ArticleDetailView, AddPostView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>',ArticleDetailView.as_view(), name='article_detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+]
---
+++ djangoBlog/djangoBlogApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
